12ba94032dea4fd72e9f7ffa88c3b6267718483b
Reagan Kirby <reaganjameskirby@gmail.com>
2023-04-15 14:43:03 -0500

    - Created model for Survey Question - Registered model in admin panel

--- app/surveys/models.py
+++ app/surveys/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class SurveyQuestion(models.Model):
+    question_title = models.CharField(max_length=50, blank=False)
+    question_body = models.CharField(max_length=200, blank=False)
+
+    def __str__(self):
+        return self.question_title
+
